8f10da4ee979f12ccd5ba5aa26a277072df6907d
widedhorchani1 <horchaniwided@gmail.com>
2023-01-24 11:42:54 +0100

    adding checkpoint React js

---
+++ src/components/Forms.js
@@ -0,0 +1,31 @@
+// import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+
+function BasicExample() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <br></br>
+        <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <br></br>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <br></br>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default BasicExample;
